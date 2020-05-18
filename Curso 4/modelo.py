8a2a47d403f65b3447cf2c776c10f77acb197ea3
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-18 13:33:49 -0300

    Aplicando o conceito básico de polimorfismo e representando textualmente os objetos

--- Curso 4/modelo.py
+++ Curso 4/modelo.py
@@ -19,14 +19,17 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def retorna_cadastro_diferenciado(self):
-        pass
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} minutos - {self._likes} Likes'
 
 
 class Serie(Programa):
@@ -34,12 +37,18 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
