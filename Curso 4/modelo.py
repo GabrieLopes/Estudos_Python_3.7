44fac57e58bff6fdb346a34b5e8529ebe3401ee3
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-19 11:39:54 -0300

    Duck Typing, Python Data Model, Dunder Methos e ABCs

--- Curso 4/modelo.py
+++ Curso 4/modelo.py
@@ -41,17 +41,19 @@ class Serie(Programa):
         return f'Nome: {self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
-class Playlist(list):
+class Playlist():
     def __init__(self, nome, programas):
         self.nome = nome
         self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
 
     @property
     def listagem(self):
         return self._programas
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
 
 
@@ -71,12 +73,11 @@ demolidor.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-filmes_e_series = [vingadores, atlanta, tmep]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
 
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
 
-print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
