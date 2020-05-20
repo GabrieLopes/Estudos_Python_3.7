53611c0071b9cd2682905b92b1e3c566ff6c958c
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-20 11:16:30 -0300

    Metodo Find

--- Curso 5/main.py
+++ Curso 5/main.py
@@ -1,3 +1,12 @@
-argumento = "www.bytebank.com/cambio?moedaorigem=real"
-subString = argumento[5:11]
-print(subString)
+# argumento = "www.bytebank.com/cambio?moedaorigem=real"
+# subString = argumento[5:11]
+# print(subString)
+
+# argumento = "moedaorigem=real"
+# index = argumento.find("=")
+# subString = argumento[index + 1:]
+# print(subString)
+
+argumento = "moedaorigem=real"
+index = argumento.split("=")
+print(index)
