6ade06662a3d619c61df421c5ff5dbd00e3e4cf1
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-22 10:53:33 -0300

    Métodos upper, lower, replace, in, startswith e endswith

--- Curso_5/ExtratorArgumentosUrl.py
+++ Curso_5/ExtratorArgumentosUrl.py
@@ -22,3 +22,35 @@ class ExtratorArgumentosUrl:
         moeda_destino = self.url[indice_inicial_moeda_destino:]
 
         return moeda_origem, moeda_destino
+
+    def retorna_moedas(self):
+        busca_moeda_origem = "moedaorigem"
+        busca_moeda_destino = "moedadestino"
+
+        inicio_substring_moeda_origem = self.encontra_indice_inicio_substring(busca_moeda_origem)
+        final_substring_moeda_origem = self.url.find("&")
+        moeda_origem = self.url[inicio_substring_moeda_origem:final_substring_moeda_origem]
+        if moeda_origem == "moedadestino":
+            moeda_origem = self.verifica_moeda_origem(busca_moeda_origem)
+
+        inicio_substring_moeda_destino = self.encontra_indice_inicio_substring(busca_moeda_destino)
+        final_substring_moeda_destino = self.url.find("&valor")
+        moeda_destino = self.url[inicio_substring_moeda_destino:final_substring_moeda_destino]
+
+        return moeda_origem, moeda_destino
+
+    def encontra_indice_inicio_substring(self, moeda_ou_valor):
+        return self.url.find(moeda_ou_valor) + len(moeda_ou_valor) + 1
+
+    def verifica_moeda_origem(self, busca_moeda_origem):
+        self.url = self.url.replace("moedadestino", "real", 1)
+        inicio_substring_moeda_origem = self.encontra_indice_inicio_substring(busca_moeda_origem)
+
+        final_substring_moeda_origem = self.url.find("&")
+        return self.url[inicio_substring_moeda_origem:final_substring_moeda_origem]
+
+    def retorna_valor(self):
+        busca_valor = "Valor".lower()
+        inicio_substring_valor = self.encontra_indice_inicio_substring(busca_valor)
+        valor = self.url[inicio_substring_valor:]
+        return valor
--- Curso_5/main.py
+++ Curso_5/main.py
@@ -1,25 +1,35 @@
 from Estudos_Python_3.Curso_5.ExtratorArgumentosUrl import ExtratorArgumentosUrl
-# argumento = "www.bytebank.com/cambio?moedaorigem=real"
-# subString = argumento[5:11]
-# print(subString)
 
-# argumento = "moedaorigem=real"
-# index = argumento.find("=")
-# subString = argumento[index + 1:]
-# print(subString)
-
-# argumento = "moedaorigem=real"
-# index = argumento.split("=")
-# print(index)
-
+# # argumento = "www.bytebank.com/cambio?moedaorigem=real"
+# # subString = argumento[5:11]
+# # print(subString)
 #
-
+# # argumento = "moedaorigem=real"
+# # index = argumento.find("=")
+# # subString = argumento[index + 1:]
+# # print(subString)
 #
-# argumentosUrl = ExtratorArgumentosUrl(url)
+# # argumento = "moedaorigem=real"
+# # index = argumento.split("=")
+# # print(index)
 #
-# moeda_origem, moeda_destino = argumentosUrl.extrair_argumentos()
-# print(moeda_destino, moeda_origem)
-url = "moedaorigem=real&moedadestino=dolar"
+# #
+#
+# #
+# # argumentosUrl = ExtratorArgumentosUrl(url)
+# #
+# # moeda_origem, moeda_destino = argumentosUrl.extrair_argumentos()
+# # print(moeda_destino, moeda_origem)
+# url = "moedaorigem=real&moedadestino=dolar"
 
+url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=150"
+cambio = ExtratorArgumentosUrl(url)
+moeda_origem, moeda_destino = cambio.retorna_moedas()
+valor = cambio.retorna_valor()
+print(moeda_origem, moeda_destino, valor)
 
-
+url = "https://www.bytebank.com.br/cambio?moedaorigem=moedadestino&moedadestino=dolar&valor=150"
+cambio = ExtratorArgumentosUrl(url)
+moeda_origem, moeda_destino = cambio.retorna_moedas()
+valor = cambio.retorna_valor()
+print(moeda_origem, moeda_destino, valor)
