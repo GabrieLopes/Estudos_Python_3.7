20036b29bb792b0c792d28084d12f438d291b0d3
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-22 11:33:39 -0300

    regex, grupos e intervalos de caracteres, quantificadores e metodos re.search, re.findall e group

---
+++ Curso_5/regex.py
@@ -0,0 +1,23 @@
+import re
+
+# padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+# padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]"
+# padrao = "[0-9]{4}[-][0-9]{4}"
+# padrao = "[0-9]{4}-[0-9]{4}"
+# padrao = "[0-9]{4,5}-[0-9]{4}"
+
+# padrao = "[a-z]{1,20}[ ][0-9]{1,2}[h]"
+# frase1 = "podemos marcar de sair sabado 23h"
+# frase2 = "acho que quinta 21h é a melhor hora para a gente ir lá"
+# frase3 = "terca 19h é um bom momento para sairmos"
+# retorno = re.search(padrao, frase1)
+# print(retorno.group())
+
+padrao = "[0-9]{4,5}-?[0-9]{4}"
+texto =  "Meu número para contato é 2633-5723"
+retorno = re.search(padrao, texto)
+print(retorno.group())
+
+
+
+
