67308c0c9f2454921bd599d0645a6d27d18e7bdf
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-05-29 13:56:13 -0300

    Tuplas

--- Curso_6/Collections.py
+++ Curso_6/Collections.py
@@ -57,14 +57,69 @@
 #     print(len(lista))
 #     print(lista)
 #     lista.append(13)
-def faz_processamento_de_visualizacao(lista=None):
-    if lista is None:
-        lista = list()
-    print(len(lista))
-    print(lista)
-    lista.append(13)
+# def faz_processamento_de_visualizacao(lista=None):
+#     if lista is None:
+#         lista = list()
+#     print(len(lista))
+#     print(lista)
+#     lista.append(13)
+#
+#
+# faz_processamento_de_visualizacao()
+# faz_processamento_de_visualizacao()
 
 
-faz_processamento_de_visualizacao()
-faz_processamento_de_visualizacao()
+# print(conta_do_gabriel)
 
+# for conta in contas:
+#     print(conta)
+# conta_do_gabriel.deposita(100)
+# class ContaCorrente():
+#     def __init__(self, codigo):
+#         self.codigo = codigo
+#         self.saldo = 0
+#
+#     def deposita(self, valor):
+#         self.saldo += valor
+#
+#     def __str__(self):
+#         return "[>>Codigo {} Saldo {}<<]".format(self.codigo, self.saldo)
+#
+#
+# def deposita_para_todas(contas):
+#     for conta in contas:
+#         conta.deposita(100)
+#
+#
+# conta_do_gabriel = ContaCorrente(15)
+# conta_do_gabriel.deposita(500)
+#
+# conta_da_dani = ContaCorrente(47685)
+# conta_da_dani.deposita(1000)
+#
+# contas = [conta_do_gabriel, conta_da_dani]
+
+# print(contas[0], contas[1])
+# deposita_para_todas(contas)
+# print(contas[0], contas[1])
+
+#
+# gabriel = (15, 100)
+# daniela = (75865, 1000)
+#
+#
+# def depositar(conta):
+#     novo_saldo = conta[1] + 100
+#     codigo = conta[0]
+#     return codigo, novo_saldo
+
+
+# gabriel = depositar(gabriel)
+
+daniela = ("Daniela", 31, 1987)
+gabriel = ("Gabriel", 19, 2000)
+
+usuarios = [daniela, gabriel]
+usuarios.append(("Paulo", 39, 1979))
+
+print(usuarios)
